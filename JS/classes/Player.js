6abc6134af919908b8b3518dff978941096df2f5
getill <theo.lemray@gmail.com>
2023-10-14 17:06:13 +0200

    ✨ We have now a player !

---
+++ JS/classes/Player.js
@@ -0,0 +1,29 @@
+// ------ Player creator ------
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+
+    this.width = 100;
+    this.height = 100;
+    this.sides = {
+      bottom: (this.position.y = this.height),
+    };
+  }
+
+  // Drawing the player on canvas
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  // Player stops when we reach the bottom
+  update() {
+    if (this.sides.bottom < canvas.height) {
+      this.position.y++;
+      this.sides.bottom = this.position.y + this.height;
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
+// let bottom = y + 100; // bottom of the player
 
 // Getting 16/9 ratio
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, 200, 200);
+const player = new Player();
+
+// ------ Animation loop ------
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.draw();
+  player.update();
+}
+
+animate();
