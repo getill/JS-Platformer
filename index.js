42f38b4b9dd154d67521e48a648a39babf9d92a3
getill <theo.lemray@gmail.com>
2023-10-15 21:12:20 +0200

    ✨ Collision blocks created and visible

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
+const parsedCollisions = collisionLevel1.parse2D();
+const collisionBlocks = parsedCollisions.createObjectsFrom2D();
+
 const backgroundLevel1 = new Sprite({
   position: {
     x: 0,
@@ -35,8 +38,11 @@ function animate() {
   window.requestAnimationFrame(animate);
 
   backgroundLevel1.draw();
-  player.velocity.x = 0;
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.draw();
+  });
 
+  player.velocity.x = 0;
   if (keys.d.pressed) {
     player.velocity.x = 5;
   } else if (keys.q.pressed) {
